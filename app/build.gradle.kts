4b0029aa04a5029a25f7cac01184149accf2026e
Marcus Becker <marcusvbecker@gmail.com>
2025-09-30 13:56:35 -0300

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.mbecker.drinkcount"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

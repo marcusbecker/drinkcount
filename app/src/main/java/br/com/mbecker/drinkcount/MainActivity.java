8c22f4ccf43d6c5e32dc2412479bcd48611112a6
Marcus Becker <marcusvbecker@gmail.com>
2025-10-09 15:45:08 -0300

    Removed deprecated code

--- app/src/main/java/br/com/mbecker/drinkcount/MainActivity.java
+++ app/src/main/java/br/com/mbecker/drinkcount/MainActivity.java
@@ -30,12 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             getWindow().getInsetsController().hide(WindowInsets.Type.navigationBars());
-        } else {
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            decorView.setSystemUiVisibility(uiOptions);
-
         }
 
         create();
